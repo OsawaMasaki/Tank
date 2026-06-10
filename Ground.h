db115aad9d5158aac941cdbc6c058ac701a971a9
将生 大澤 <250631@jc-21.jp>
2026-06-10 12:02:16 +0900

    レイを地面にとばしてタンクを落とした

--- Ground.h
+++ Ground.h
@@ -12,6 +12,7 @@ public:
     void Update() override;
     void Draw() override;
     void Release() override;
+    int GetModelHandle() { return hModel_; }
 private:
     int hModel_;//地形モデルのハンドル
 };
--- Tank.cpp
+++ Tank.cpp
@@ -2,13 +2,14 @@
 #include "Engine/Model.h"
 #include "Engine/Input.h"
 #include "Engine/Debug.h"
+#include "Ground.h"
 
 //タンクのボディのクラス
 
 namespace
 {
-	XMVECTOR vFront = { 0,0,1,0 };   //タンクの前方向のベクトル
-	float moveSpeed = 0.5f;
+	XMVECTOR vFront = { 0,0,1,0 };  //タンクの前方向のベクトル
+	float moveSpeed = 0.1f;         //タンクの移動速度
 
 }
 
@@ -35,28 +36,28 @@ void Tank::Update()
 {
 	//回転の処理
 	//Aキーを押している間、左に回転する
-	if(Input::IsKey(DIK_A))
+	if (Input::IsKey(DIK_A))
 	{
-		transform_.rotate_.y += 2.0f;
+		transform_.rotate_.y -= 2.0f;
 	}
 	//Dキーを押している間、右に回転する
 	if (Input::IsKey(DIK_D))
 	{
-		transform_.rotate_.y -= 2.0f;
+		transform_.rotate_.y += 2.0f;
 	}
 
 
 	//移動の処理
-	if (Input::IsKey(DIK_UP)|| Input::IsKey(DIK_W))
+	if (Input::IsKey(DIK_UP) || Input::IsKey(DIK_W))
 	{
 		//transform_.position_.z += 0.1f;
-		
-		 XMVECTOR vPos = XMLoadFloat3(&transform_.position_);//ロード：読み込み
-		 XMMATRIX matRot = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.y));//Y軸回転行列を作る
-		 XMVECTOR vMove = XMVector3TransformCoord(vFront, matRot);
 
-		 vPos = vPos + moveSpeed * vMove;
-		 XMStoreFloat3(&transform_.position_, vPos); //ストア：書き込み
+		XMVECTOR vPos = XMLoadFloat3(&transform_.position_);//ロード：読み込み
+		XMMATRIX matRot = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.y));//Y軸回転行列を作る
+		XMVECTOR vMove = XMVector3TransformCoord(vFront, matRot);
+
+		vPos = vPos + moveSpeed * vMove;
+		XMStoreFloat3(&transform_.position_, vPos); //ストア：書き込み
 	}
 	/*
 	if (Input::IsKey(DIK_DOWN))
@@ -75,7 +76,26 @@ void Tank::Update()
 
 	//デバック用
 	Debug::Log("Yangle = ");
-	Debug::Log(transform_.rotate_.y,true);
+	Debug::Log(transform_.rotate_.y, true);
+
+
+	//レイキャストして、浮いてたら地面に落とす
+	RayCastData data;
+	data.start = transform_.position_;
+	data.start.y = 0.0f;   //地面は0より下に作られている。そうじゃないときはもっと上から！
+	data.dir = { 0, -1,0 };//真下にレイをとばす
+
+	//groundオブジェクトのモデルのハンドルを探す
+	Ground* pGround = (Ground*)FindObject("Ground");
+
+	int hGroundModel = pGround->GetModelHandle();
+	Model::RayCast(hGroundModel, &data);
+
+	if (data.hit == true)
+	{
+		//レイの発射位置から、地面までの距離を引いて、地面にぴったりつける
+		transform_.position_.y = - data.dist;
+	}
 }
 
 void Tank::Draw()
